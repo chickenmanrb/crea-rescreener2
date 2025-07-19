9c28d1f15379318f617bfb88b218d9bf49600510
chickenmanrb <41556160+chickenmanrb@users.noreply.github.com>
2025-07-18 20:14:03 -0500

    Fix Tailwind CSS configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Import the plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add the plugin to the plugins array
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 })
