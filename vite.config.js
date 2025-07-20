a44925f09963ea17c5f760b66f338ed4a34dee6b
chickenmanrb <41556160+chickenmanrb@users.noreply.github.com>
2025-07-20 13:43:10 -0500

    Fix local development to use Netlify CLI

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/.netlify/functions': {
-        target: 'https://merry-blancmange-29b5cd.netlify.app',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
   server: {
     port: 5173,
     host: true
